d13e69590a4f8f25cfc9d28ca9a32dfdfaa250cc
vitor <vitor.oliveira.rolla@gmail.com>
2025-11-14 14:44:28 -0300

    Programa 2

--- AC_2/Exercícios_Práticos/Mars - Máquinas/Programas de Máquina/programa_2.asm
+++ AC_2/Exercícios_Práticos/Mars - Máquinas/Programas de Máquina/programa_2.asm
@@ -18,20 +18,11 @@
 .globl main
 
 main: 
-
-	# Atribuição de valores
-	ori $s0, $zero, 2 # a
-	ori $s1, $zero, 3 # b
-	ori $s2, $zero, 4 # c
-	ori $s3, $zero, 5 # d
 	
-	# Somando valores
-	add $t0, $s0, $s1 # Atribuição -> t0: s0 + s1 (a + b)
-	add $t1, $s2, $s3 # Atribuição -> t1: s2 + s3 (c + d)
-	sub $s4, $t0, $t1 # Atribuição -> (x)s4: t0 - t1 
-	
-	sub $t3, $s0, $s1 # Atribuição -> t3: s0 - s1 (a - b)
-	add $s5, $t3, $s4 # Atribuição -> (y)s5: t3 - s4 (t3 + x)
-	sub $s1, $s4, $s5 # Atribuição -> s1 = x - y
+	ori  $s0, $zero, 1 # Atribuição -> x = 1
+	add  $t0, $s0, $s0 # Atribuição -> t0 = 2x
+	add  $t0, $t0, $t0 # Atribuição -> t0 = 4x
+	add  $t0, $t0, $s0 # Atribuição -> t0 = 5x
+	addi $s1, $t0, 15  # Atribuição -> y = 5x + 15
 	
 	
