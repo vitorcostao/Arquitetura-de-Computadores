df56eb74cb220f71fde08f3b8ac6fd12aafbcd30
vitor <vitor.oliveira.rolla@gmail.com>
2025-11-14 19:22:06 -0300

    Programa 12

--- AC_2/Exercícios_Práticos/Mars - Máquinas/Programas de Máquina/programa_12.asm
+++ AC_2/Exercícios_Práticos/Mars - Máquinas/Programas de Máquina/programa_12.asm
@@ -4,13 +4,13 @@
 main:
 
 	
-	la $t0, x
-	lw $t1, ($t0)    # $t1 = endereço de ptr2
-	lw $t2, ($t1)    # $t2 = endereço de ptr1  
-	lw $t3, ($t2)    # $t3 = endereço de k
-	lw $t4, ($t3)    # $t4 = valor de k (2) ← FALTA ESTA LINHA!
-	sll $t4, $t4, 1  # $t4 = 4
-	sw $t4, ($t3)    # armazena 4 em k
+	la $t0, x # Endereço de X
+	lw $t1, ($t0) # t1 = endereço de ptr2
+	lw $t2, ($t1) # t2 = endereço de ptr1  
+	lw $t3, ($t2) # t3 = endereço de k
+	lw $t4, ($t3) # t4 = valor
+	sll $t4, $t4, 1 # t4 = 4
+	sw $t4, ($t3) # armazena 4 em k
 	
 	
 	
